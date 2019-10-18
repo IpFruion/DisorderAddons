94b72e984393f922af846df41d12d560e795a50d
IpFruion <IpFruion@users.noreply.github.com>
2019-10-18 17:58:34 -0500

    Added Begone Addon
    Has No functionality just empy with some libraries that might be used.

---
+++ Begone/Locale/ruRU.lua
@@ -0,0 +1,64 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Caterer", "ruRU", false)
+if not L then return end
+
+--Food
+L["Conjured Cinnamon Roll"] = "Сотворенная плюшка с корицей"
+L["Conjured Sweet Roll"] = "Сотворенная сладкая булочка"
+L["Conjured Sourdough"] = "Сотворенный батон"
+L["Conjured Pumpernickel"] = "Сотворенный хлеб грубого помола"
+L["Conjured Rye"] = "Сотворенный ржаной хлеб"
+L["Conjured Bread"] = "Сотворенный хлеб"
+L["Conjured Muffin"] = "Сотворенный кекс"
+--Water
+L["Conjured Crystal Water"] = "Сотворенная талая вода"
+L["Conjured Sparkling Water"] = "Сотворенная шипучка"
+L["Conjured Mineral Water"] = "Сотворенная минеральная вода"
+L["Conjured Spring Water"] = "Сотворенная ключевая вода"
+L["Conjured Purified Water"] = "Сотворенная очищенная вода"
+L["Conjured Fresh Water"] = "Сотворенная свежая вода"
+L["Conjured Water"] = "Сотворенная вода"
+
+L["Enable addon"] = 'Включить аддон'
+L["Trade with friends"] = "Обмен с друзьями"
+L["Trade with group"] = "Обмен с членами группы/рейда"
+L["Trade with guild"] = "Обмен с членами гильдии"
+L["Trade with other"] = "Обмен с другими"
+--L["Whisper requests"] = "Запросы шепотом"
+
+L["General"] = 'Общее'
+L["Class filter"] = 'Фильтр классов'
+L["Exception list"] = 'Список исключений'
+
+L["On"] = "Вкл."
+L["Off"] = "Выкл."
+L["Player name"] = 'Имя игрока'
+L["Class"] = "Класс"
+L["Food"] = "Еда"
+L["Water"] = "Вода"
+L['Add'] = 'Добавить'
+L['Edit'] = 'Изменить'
+L["Note"] = "Примечание"
+L["Expected string"] = "Ожидается строка"
+
+L["added"] = 'добавлен'
+L["edited"] = 'изменен'
+L["removed"] = 'удален'
+
+L["<player name>"] = "<имя игрока>"
+L["<amount of food>"] = "<кол-во еды>"
+L["<amount of water>"] = "<кол-во воды>"
+L["Player <%s> was successfully %s."] = "Игрок <%s> был успешно %s."
+L["Enter the correct name."] = "Введите правильное имя."
+L["The numbers must be a multiple of 20."] = "Число должно быть кратным 20."
+--L["Service is temporarily disabled."] = "Сервис временно отключен."																																												 
+L["The total number of items should not exceed 120."] = "Общее количество предметов не должно превышать 120."
+L["It is necessary to come closer."] = "Необходимо подойти ближе."
+L["You are on my blacklist."] = "Вы в моем черном списке."
+L["I can't complete the trade right now. I'm out of %s."] = "Я не могу завершить обмен сейчас. У меня недостаточно %s."
+L["Had a problem picking things up!"] = "Имеется проблема со взятием предмета!"
+L["To add the current target, instead of the name, you can write '%t'."] = "Для добавления текущей цели, вместо имени можно записать '%t'."
+L["If you want to ignore the player, then set the quantity for both items to zero."] = "Если вы хотите игнорировать игрока, то установите количество для обоих предметов на нуль."
+
+L["Right click on the minimap button to open settings.\nClick on the point tooltip to quickly manage the addon."] = "Нажмите ПКМ на кнопку миникарты, чтобы открыть настройки.\nНажмите на пункт в подсказке для быстрого управления аддоном."
+L["Do you really want to reset the settings to their default values?"] = "Вы действительно хотите сбросить настройки до значений по умолчанию?"
+L["All settings are reset to default values."] = "Все настройки сброшены до значений по умолчанию."
